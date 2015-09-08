758651f11777271e6c5aba744efe3ea7cc256702
cbb294609622 <294609622@qq.com>
2015-09-08 15:19:30 +0800

    修改了一些地方

--- app/src/main/java/view/turingrobot/com/myturingrobot/MainActivity.java
+++ app/src/main/java/view/turingrobot/com/myturingrobot/MainActivity.java
@@ -3,8 +3,10 @@ package view.turingrobot.com.myturingrobot;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import view.turingrobot.com.myturingrobot.utils.MyToastUitl;
@@ -15,6 +17,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
         MyToastUitl.showToastFlag(mContext,"主界面");
